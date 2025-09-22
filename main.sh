a1691786e8a645bde0e64323b63883ef1bac0cc2
Juban Do Rego <jubanreal500@gmail.com>
2025-09-22 18:00:49 +0100

    vscode

--- main.sh
+++ main.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 cd velocity
 
+DIVIDER = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+echo $DIVIDER 
+
 echo "Your MOTD (eagler only): $MOTD"
 echo "Your Max Players: $MAXPLAYERS"
 echo "Your IP (server): $SERVER"
+
+echo $DIVIDER
 
 sed -i 's/${MOTD}/'"$MOTD"'/g' velocity.toml
 sed -i 's/${MAXPLAYERS}/'"$MAXPLAYERS"'/g' velocity.toml
 sed -i 's/${SERVER}/'"$SERVER"'/g' velocity.toml
 
-cd plugins
-cd eaglerxserver
+cd plugins/eaglerxserver
 
 sed -i 's/${MOTD}/'"$MOTD"'/g' listeners.toml
 
-cd ..
-cd ..
+cd ../../..
+
+chmod +x pinger.sh
+./pinger.sh &
+
+cd velocity
 
 echo "Starting Velocity!"
 java -Xmx512M -Xms512M -jar velocity.jar
