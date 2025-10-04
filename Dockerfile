3b23cb005b7bc04c4199a0a2f8ae544122bad00e
devJuban <jubanreal500@gmail.com>
2025-10-04 15:44:36 +0100

    Update main.sh

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:17-jdk-jammy
+FROM eclipse-temurin:21-jdk
 COPY . .
 RUN chmod +x main.sh
 CMD ["./main.sh"]
--- main.sh
+++ main.sh
@@ -2,20 +2,18 @@
 cd velocity
 
 echo "Your MOTD (eagler only): $MOTD"
-echo "Your Max Players: $MAXPLAYERS"
-echo "Your IP (server): $SERVER"
+echo "Your Max Players (eagler only): $MAXPLAYERS"
+echo "Your IP: $SERVER"
 
 sed -i 's/${MOTD}/'"$MOTD"'/g' velocity.toml
 sed -i 's/${MAXPLAYERS}/'"$MAXPLAYERS"'/g' velocity.toml
 sed -i 's/${SERVER}/'"$SERVER"'/g' velocity.toml
 
-cd plugins
-cd eaglerxserver
+cd plugins/eaglerxserver
 
 sed -i 's/${MOTD}/'"$MOTD"'/g' listeners.toml
 
-cd ..
-cd ..
+cd ../..
 
-echo "Starting Velocity"
+echo "Starting Velocity | For Render"
 java -Xmx512M -Xms512M -jar velocity.jar
